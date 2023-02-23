d199b73a0f33919e2d8bd4efa600df06bdab812e
Juan Diego Luna Cazares <diego.luna@seacoahuila.org.mx>
2023-02-23 17:16:17 -0600

    Arreglar  Usuarios Store

--- src/stores/usuarios.js
+++ src/stores/usuarios.js
@@ -1,9 +1,9 @@
 import { defineStore } from "pinia";
-import { updateEmail, updatePassword, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
+import { updateEmail, updatePassword, createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth, db } from '../firebase.js'
 import { query, collection, where, getDocs, setDoc, doc } from "firebase/firestore/lite";
 
-export const useCurrentUserStore = defineStore('CurrentUser',{
+export const useUsuariosStore = defineStore('UsuariosStore',{
     state: () => ({
         loading: false,
         message:{
@@ -12,20 +12,31 @@ export const useCurrentUserStore = defineStore('CurrentUser',{
             text:'',
             place:null
         },
-        id:null,
-        cargo: null,
-        email: null,
-        nombre: null,
-        rol: null,
-        uid: null,
-        modIds: [],
-        modulos: [],
+        listado:[],
+        
     }),
     actions:{
         
         async getAll(){
             try {
                 this.loading = true;
+                this.listado = [];
+
+                await getDocs(
+                    query(
+                        collection(db, 'usuarios'),
+                    )
+                ).then((datos) => {
+                    
+                    datos.docs.forEach((doc) => {
+                        this.listado.push({ id: doc.id, ...doc.data() })
+                    });
+
+                }).catch((e) => {
+                    console.log(e.message);
+                    this.setError(e.message);
+                })
+
             } catch (error) {
                 this.setError(error.message);
             } finally {
@@ -81,6 +92,23 @@ export const useCurrentUserStore = defineStore('CurrentUser',{
             } finally {
                 this.loading = false;
             }
+        },
+
+
+
+
+        setError(msg = ''){
+            this.message.error = true;
+            this.message.success = false;
+            this.message.text = msg;
+            setTimeout(() => { this.message.error = false; this.message.text = ''; }, 6000);
+        },
+
+        setSuccess(msg = ''){
+            this.message.success = true;
+            this.message.error = false;
+            this.message.text = msg;
+            setTimeout(() => { this.message.success = false; this.message.text = ''; }, 6000);
         },
 
     }
--- src/views/usuarios/Listado.vue
+++ src/views/usuarios/Listado.vue
@@ -1,41 +1,23 @@
 <script>
 import { useCurrentUserStore } from '../../stores/currentUser';
 import router from '../../router';
-const currentUser = useCurrentUserStore();
+import { useUsuariosStore } from '../../stores/usuarios';
 
+const currentUser = useCurrentUserStore();
+const usuarios = useUsuariosStore();
 
 async function cargarDatos(){
     if(currentUser.id == null){ currentUser.getDatos() }
-    console.log(currentUser.loading)
+    await usuarios.getAll();
+    
 }
 cargarDatos();
 </script>
 <template>
     
     <DefaultPage>
-        <div>
-            <PageTitle><Icon name="people-fill"/> &nbsp;Usuarios</PageTitle>
-
-            <div class="row">
-                <div class="col">
-                    <Card>
-                    <CardHeader>
-                        Usuarios registrados 10:
-                    </CardHeader>
-                    <CardBody>
-                        <div class="list-group" >
-                            <RouterLink v-for="i in 10" class="list-group-item" to="/">
-                            <div class="row">
-                                <div class="col-6">Hola</div>
-                                <div class="col-6 float-right">Mundo</div>
-                            </div>    
-                            </RouterLink>
-                        </div>
-                    </CardBody>
-                </Card>
-            </div>
-        </div>
-        </div>
+        
+        <Loading v-if="usuarios.loading"></Loading>
 
     </DefaultPage>
 
