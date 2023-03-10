09e5effef54b40fd31f3fa080b38c08f78a8b3af
Juan Diego Luna Cazares <diego.luna@seacoahuila.org.mx>
2023-03-10 12:30:00 -0600

    Configuración de la base de datos de prueba

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,31 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 import { getAuth } from "firebase/auth";
 
+
+/**
+ * Base de datos de producción:
+ */
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDIS5dpFmXD6qjmH51HrQ_UMmKWtRvKKxc",
+//   authDomain: "transparenciaseac.firebaseapp.com",
+//   projectId: "transparenciaseac",
+//   storageBucket: "transparenciaseac.appspot.com",
+//   messagingSenderId: "640440931494",
+//   appId: "1:640440931494:web:81e2d4d8a42d3422a0d067"
+// };
+
+
+
+/**
+ * Base de datos de pruebas:
+ */
 const firebaseConfig = {
-  apiKey: "AIzaSyDIS5dpFmXD6qjmH51HrQ_UMmKWtRvKKxc",
-  authDomain: "transparenciaseac.firebaseapp.com",
-  projectId: "transparenciaseac",
-  storageBucket: "transparenciaseac.appspot.com",
-  messagingSenderId: "640440931494",
-  appId: "1:640440931494:web:81e2d4d8a42d3422a0d067"
+  apiKey: "AIzaSyCTuFryyv-L7EmkoKAKNuwgg9JcsVoHnzE",
+  authDomain: "pruebastransparenciaseac.firebaseapp.com",
+  projectId: "pruebastransparenciaseac",
+  storageBucket: "pruebastransparenciaseac.appspot.com",
+  messagingSenderId: "496952588393",
+  appId: "1:496952588393:web:ec1ddb266e1353f2994af8"
 };
 
 initializeApp(firebaseConfig);
