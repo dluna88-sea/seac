9b13a96044664e3b30160a8930601e29065cd5ad
Juan Diego Luna Cazares <diego.luna@seacoahuila.org.mx>
2023-04-17 10:07:29 -0600

    Cambios en autores

---
+++ src/components/modals/boletines/autores/EnlaceAutor.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="modal fade" id="enlaceAutor" aria-hidden="true" aria-labelledby="enlaceAutorLabel" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="nuevoBoletinModalLabel">
+                        Agregar Enlace
+                    </h1>
+                </div>
+                <div class="modal-body">
+                    Agregar enlace aquí
+                </div>
+                <div class="modal-footer">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/boletines/Autores.vue
+++ src/views/boletines/Autores.vue
@@ -18,46 +18,79 @@ cargarAutores()
     <DefaultPage>
         
         <PageTitle :bread="bread">
-            <Icon name="people" Class="mx-2" />Autores
+            <Icon name="people-fill" Class="mx-2" />Autores
         </PageTitle>
         
         <Loading v-if="autores.loading"></Loading>
-        <div v-else>
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="list-group">
+        <div class="mb-5" v-else>
+            <div class="row mb-5">
+                <div class="col-md-7 mb-5">
+                    <Info v-if="autores.listado.length == 0">No hay autores registrados</Info>
+                    <div v-else class="list-group shadow-sm">
                         <router-link v-for="autor in autores.listado" :to="`/boletines/autor/${autor.id}`" class="list-group-item">{{ autor.nombre }}</router-link>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <h4>Registrar nuevo</h4><hr>
                     <form name="nuevoAutorForm">
                         <div class="mt-2">
                             <label for="nombre">Nombre:</label>
-                            <input type="text" name="nombre" class="form-control mt-2">
-                        </div>
-                        <div class="mt-2">
-                            <label for="nombre">Cargo o Título:</label>
-                            <input type="text" name="cargo" class="form-control mt-2">
+                            <input autocomplete="off" type="text" name="nombre" class="form-control mt-2">
                         </div>
                         <div class="mt-2">
                             <label for="nombre">Resumen o biografía:</label>
-                            <textarea name="cargo" class="form-control mt-2"></textarea>
+                            <textarea autocomplete="off" name="cargo" class="form-control mt-2"></textarea>
                         </div>
                         <hr>
                         <div class="mt-2">
-                            <label for="nombre">Enlaces:</label>
+                            <label>Enlaces:</label><br>
                             <div class="input-group mt-2">
                                 <span class="input-group-text">
-                                    <Icon name="facebook" />
+                                    <Icon name="envelope" />
                                 </span>
-                                <input type="text" class="form-control" placeholder="Username">
+                                <input type="text" class="form-control" placeholder="Dirección de correo electrónico">
+                            </div>
+                            <div class="input-group mt-2">
+                                <span class="input-group-text">
+                                    <Icon name="globe" />
+                                </span>
+                                <input type="text" class="form-control" placeholder="Pega el enlace del sitio web">
                             </div>
                             <div class="input-group mt-2">
                                 <span class="input-group-text">
                                     <Icon name="twitter" />
                                 </span>
-                                <input type="text" class="form-control" placeholder="Username">
+                                <input type="text" class="form-control" placeholder="Pega el enlace de twitter">
+                            </div>
+                            <div class="input-group mt-2">
+                                <span class="input-group-text">
+                                    <Icon name="facebook" />
+                                </span>
+                                <input type="text" class="form-control" placeholder="Pega el enlace de facebook">
+                            </div>
+                            <div class="input-group mt-2">
+                                <span class="input-group-text">
+                                    <Icon name="github" />
+                                </span>
+                                <input type="text" class="form-control" placeholder="Pega el enlace de github">
+                            </div>
+                            <div class="input-group mt-2">
+                                <span class="input-group-text">
+                                    <Icon name="linkedin" />
+                                </span>
+                                <input type="text" class="form-control" placeholder="Pega el enlace de linkedin">
+                            </div>
+                            <div class="input-group mt-2">
+                                <span class="input-group-text">
+                                    <Icon name="instagram" />
+                                </span>
+                                <input type="text" class="form-control" placeholder="Pega el enlace de instagram">
+                            </div>
+                            <div class="input-group mt-2">
+                                <span class="input-group-text">
+                                    <Icon name="tiktok" />
+                                </span>
+                                <input type="text" class="form-control" placeholder="Pega el enlace de tiktok">
                             </div>
                         </div>
                     </form>
