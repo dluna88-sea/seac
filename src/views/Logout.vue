10053785e7d83ec669d90f0272769fc167354086
Diego Luna <diego.luna@seacoahuila.org.mx>
2024-06-06 10:52:06 -0600

    Logout corregido

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,18 +1,11 @@
 <script setup>
-import router from '../router'
-import { useCurrentUserStore } from '../stores/currentUser';
-const currentUser = useCurrentUserStore();
+import { auth } from '../firebase.js'
+import { signOut } from 'firebase/auth';
 
 async function salir(){
     try{
-
-        if(currentUser.id != undefined){
-            await currentUser.logout();
-            setTimeout(() => { location.href="/" },2000)
-        }else{
-            location.href='/';
-        }
-
+        await signOut(auth)
+        location.href='/';
     } catch(e){ console.log(e); }
 }
 
